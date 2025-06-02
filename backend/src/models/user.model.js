b333646ff8ffa8ad21330f3d15725c1448d36878
Nitin Reddy <nitinreddy095@gmail.com>
2025-06-02 20:31:39 +0530

    additional functions added in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
     name:{
-        type:String,
-        required:true
+        type:String
     },
     email: {
         type: String,
@@ -20,8 +21,46 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
+    refreshToken:String
 
 },{timestamps:true})
 
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password=await bcrypt.hash(this.password,10)
+        next()
+    }
+    else{
+        return next()
+    }
+})
+
+userSchema.methods.checkPassword = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            email:email.toLowerCase()
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            email:email.toLowerCase()
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User",userSchema)
